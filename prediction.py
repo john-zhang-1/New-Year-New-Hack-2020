d4f2c03e8a3ed43fa1a1606223d259d94c9fc0ae
john-zhang-1 <76273385+john-zhang-1@users.noreply.github.com>
2021-01-02 16:12:31 -0500

    Delete prediction.py

--- prediction.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential, save_model, load_model
-model = load_model('Example Model/deep_neural_network_model', compile = True)
-
-features = [[108, 160, 341, 181, 21, 2, 21, 49, 22]]
-
-prediction = model.predict(features)
-print(prediction)
-
