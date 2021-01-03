7f1a933d5b16308efab4358b347650390eb3d633
john-zhang-1 <76273385+john-zhang-1@users.noreply.github.com>
2021-01-03 00:35:24 -0500

    Update Generate Example Data.py

--- new_hack_backend/Generate Example Data.py
+++ new_hack_backend/Generate Example Data.py
@@ -1,24 +1,29 @@
 import csv
 import random
 
-variables = ['Meal', 'Bedtime', 'Waketime', 'Length', 'Quality', 'Electronics', 'Up', 'Temperature', 'Noise', 'Nap']
+variables = ['Meal', 'Bedtime', 'Waketime', 'Quality', 'Electronics', 'Up', 'Temperature', 'Noise', 'Nap']
 
-standards = [120, 1380, 480, 540, 0, 15, 1, 23, 30, 15]
-noise_max = [60, 240, 240, 0, 0, 15, 2, 4, 30, 15]
-weighting = [0.5, 1.5, 1.5, 3, 0, 0.5, 1, 1, 1, 1]
+standards = [120, 1380, 480, 0, 15, 1, 23, 30, 15]
+noise_max = [60, 240, 240, 0, 15, 2, 4, 30, 15]
+weighting = [0.5, 1.5, 1.5, 0, 0.5, 1, 1, 1, 1]
 
 def create_doc():
     with open('Example Model/random_gen_data.csv', mode = 'w') as myfile:
+
+        quality_ind = variables.index('Quality')
+        up_ind = variables.index('Up')
+        bedtime_ind = variables.index('Bedtime')
+
         writer = csv.writer(myfile)
         writer.writerow(variables)
 
-        for i in range(100):
+        for day in range(100):
 
             new_point = []
             for i in standards:
                 new_point.append(i)
 
-            for i in range(10):
+            for i in range(len(variables)):
                 noise = random.random() * noise_max[i]
 
                 if random.random() > 0.5:
@@ -26,19 +31,18 @@ def create_doc():
                 else:
                     new_point[i] -= noise
 
-                if i == 1 and new_point[1] >= 1440:
-                    new_point[1] -= 1440
+                if i == bedtime_ind and new_point[bedtime_ind] >= 1440:
+                    new_point[bedtime_ind] -= 1440
 
-            new_point[6] = round(abs(new_point[6]))
-            new_point[3] = new_point[2] - new_point[1] if new_point[2] >= new_point[1] else new_point[2] - new_point[1] + 1440
-            new_point[4] = 0
+            new_point[up_ind] = round(abs(new_point[up_ind]))
+
             for i in range(len(variables)):
-                if i != 4:
-                    new_point[4] += 1.75 * random.random() * (1 - abs(standards[i] - new_point[i])/standards[i]) * weighting[i]
+                if i != quality_ind:
+                    new_point[quality_ind] += 2.25 * random.random() * (1 - abs((standards[i] - new_point[i])/standards[i])) * weighting[i]
 
-            if new_point[4] > 10:
-                new_point[4] = 10
-            new_point[4] = round(new_point[4])
+            if new_point[quality_ind] > 10:
+                new_point[quality_ind] = 10
+            new_point[quality_ind] = round(new_point[quality_ind])
 
             writer.writerow(new_point)
 
